0bc53195dd4ec9b51adafcb7bdad60e6cebe415b
albertoduqe <albertoduqe@gmail.com>
2018-07-26 12:22:21 +0200

    Bring back RRO_TARGET frameworks-res

--- msm8996.mk
+++ msm8996.mk
@@ -24,8 +24,8 @@ $(call inherit-product, vendor/xiaomi/msm8996-common/msm8996-common-vendor.mk)
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
 
-#PRODUCT_ENFORCE_RRO_TARGETS := \
-#    framework-res
+PRODUCT_ENFORCE_RRO_TARGETS := \
+    framework-res
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
